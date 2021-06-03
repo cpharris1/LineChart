1dae20cfaa149742b4315d1bd0487de979b7c2f3
Chloe <cpharris1@crimson.ua.edu>
2021-06-03 14:17:50 -0500

    add dynamic zoom

--- line.js
+++ line.js
@@ -1,17 +1,3 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 1200 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#line_plot")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
 // Sampling Rate
 const freq = 512; // Hz
 
@@ -56,57 +42,145 @@ function getEEGData(channelNames, data)
     return d;
 }
 
-d3.csv("A114_raw_512HZ.csv", 
-  function(d) {
-    return d;
-  }, 
 
-  function(error, data) {
+
+function draw(data, id){
+
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 30, bottom: 30, left: 60},
+  width = 1200 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  var svg = d3.select('#'+id)
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.time; }))
+    .range([ 0, width ]);
+  xAxis = svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+        
+  // text label for the x axis
+  svg.append("text")             
+  .attr("transform",
+        "translate(" + (width/2) + " ," + 
+                       (height + margin.top + 20) + ")")
+  .style("text-anchor", "middle")
+  .text("Time");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.amplitude; }))
+    .range([ height, 0 ]);
+  yAxis = svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // text label for the y axis
+  svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("Amplitude");      
+
+  // Add a clipPath: everything out of this area won't be drawn.
+  var clip = svg.append("defs").append("svg:clipPath")
+      .attr("id", "clip")
+      .append("svg:rect")
+      .attr("width", width )
+      .attr("height", height )
+      .attr("x", 0)
+      .attr("y", 0);
+
+  // Add brushing
+  var brush = d3.brushX()                   // Add the brush feature using the d3.brush function
+      .extent( [ [0,0], [width,height] ] )  // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+      .on("end", updateChart)               // Each time the brush selection changes, trigger the 'updateChart' function
+
+  // Create the line variable: where both the line and the brush take place
+  var line = svg.append('g')
+    .attr("clip-path", "url(#clip)")
+
+  // Add the line
+  line.append("path")
+    .datum(data)
+    .attr("class", "line")  // I add the class line to be able to modify this line later on.
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function(d) { return x(d.time) })
+      .y(function(d) { return y(d.amplitude) })
+      )
+
+  // Add the brushing
+  line
+    .append("g")
+      .attr("class", "brush")
+      .call(brush);
+
+  // A function that set idleTimeOut to null
+  var idleTimeout
+  function idled() { idleTimeout = null; }
+
+  // A function that update the chart for given boundaries
+  function updateChart() {
+
+    // What are the selected boundaries?
+    extent = d3.event.selection
+
+    // If no selection, back to initial coordinate. Otherwise, update X axis domain
+    if(!extent){
+      if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+      x.domain([ 4,8])
+    }else{
+      x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+      line.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+    }
+
+    // Update axis and line position
+    xAxis.transition().duration(1000).call(d3.axisBottom(x))
+    line
+        .select('.line')
+        .transition()
+        .duration(1000)
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.time) })
+          .y(function(d) { return y(d.amplitude) })
+        )
+  }
+
+  // If user double click, reinitialize the chart
+  svg.on("dblclick",function(){
+    x.domain(d3.extent(data, function(d) { return d.time; }))
+    xAxis.transition().call(d3.axisBottom(x))
+    line
+      .select('.line')
+      .transition()
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.time) })
+        .y(function(d) { return y(d.amplitude) })
+    )
+  });
+  
+}
+
+
+d3.csv("A114_raw_512HZ.csv", 
+  function(data) {
 
     // Parse out channel names
     var channelNames = d3.keys(data[0]);
-    console.log(channelNames);
-
-    // Transpose data. Return Array of objects with channel and array of amplitudes
-    let allData = getEEGData(channelNames, data);
-    console.log(allData);
-
-    // Extract the channel name and the amplitudes
-    console.log(allData[0].channel);
-    console.log(allData[0].value);
     
-    let amplitude1 = getAmplitude(allData[0].value);
-    let time1 = getTime(allData[0].value);
-    console.log(amplitude1);
-    console.log(time1);
+    let channelData = getChannelData(channelNames[0], data);
+    draw(channelData, "line_plot1");
 
-    let goodData = getChannelData(channelNames[0], data);
-    console.log(goodData);
-
-     // Add X axis --> it is a date format
-     var x = d3.scaleLinear()
-     .domain(d3.extent(goodData, function(d) { return d.time; }))
-     .range([ 0, width ]);
-   svg.append("g")
-     .attr("transform", "translate(0," + height + ")")
-     .call(d3.axisBottom(x));
-
-   // Add Y axis
-   var y = d3.scaleLinear()
-     .domain(d3.extent(goodData, function(d) { return d.amplitude; }))
-     .range([ height, 0 ]);
-   svg.append("g")
-     .call(d3.axisLeft(y));
-
-   // Add the line
-   svg.append("path")
-     .datum(goodData)
-     .attr("fill", "none")
-     .attr("stroke", "steelblue")
-     .attr("stroke-width", 1.5)
-     .attr("d", d3.line()
-       .x(function(d) { return x(d.time) })
-       .y(function(d) { return y(d.amplitude) })
-       )
-  });
-
+});
